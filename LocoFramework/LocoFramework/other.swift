9a9a5ed04484ecf48f936dc07ee1922efb2e2a42
Alberto Gonzalez <jgonzalez@ia.com.mx>
2021-05-04 17:21:31 -0500

    Delete example

---
+++ LocoFramework/LocoFramework/other.swift
@@ -0,0 +1,18 @@
+//
+//  other.swift
+//  LocoFramework
+//
+//  Created by José Alberto González Gordillo on 04/05/21.
+//  Copyright © 2021 José Alberto González Gordillo. All rights reserved.
+//
+
+import Foundation
+
+public class other {
+
+    private init() { }
+
+    public static func someThings() -> String {
+        return ""
+    }
+}
--- Tizzestin Dat Frizzy/Pods/Target Support Files/Pods-Tizzestin Dat Frizzy/Pods-Tizzestin Dat Frizzy-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tizzestin_Dat_Frizzy : NSObject
-@end
-@implementation PodsDummy_Pods_Tizzestin_Dat_Frizzy
-@end
