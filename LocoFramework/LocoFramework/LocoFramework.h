a75382f6515af80501ea7be4493e5cd52b57bef9
Alberto Gonzalez <jgonzalez@ia.com.mx>
2021-05-04 16:40:17 -0500

    Finished our Loco framework

---
+++ LocoFramework/LocoFramework/LocoFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocoFramework.h
+//  LocoFramework
+//
+//  Created by José Alberto González Gordillo on 27/04/21.
+//  Copyright © 2021 José Alberto González Gordillo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocoFramework.
+FOUNDATION_EXPORT double LocoFrameworkVersionNumber;
+
+//! Project version string for LocoFramework.
+FOUNDATION_EXPORT const unsigned char LocoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFramework/PublicHeader.h>
+
+
---
+++ LocoFramework/LocoFramework/Services.swift
@@ -0,0 +1,18 @@
+//
+//  Services.swift
+//  LocoFramework
+//
+//  Created by José Alberto González Gordillo on 27/04/21.
+//  Copyright © 2021 José Alberto González Gordillo. All rights reserved.
+//
+
+import Foundation
+
+public class Services {
+
+    private init() { }
+
+    public static func someThings() -> String {
+        return ""
+    }
+}
